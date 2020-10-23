69fc7ba6763243438c9dfac7584a2d5ceb6f8012
Anna <rosalenanna@gmail.com>
2020-10-23 17:51:17 +0200

    impostazione lettera maiuscola

--- js/myscript.js
+++ js/myscript.js
@@ -2,12 +2,14 @@
 
 // Chiedere all’utente il cognome
 var cognomeUtente = prompt("inserisci il tuo cognome");
+var cognomeCapitalized = cognomeUtente.charAt(0).toUpperCase() + cognomeUtente.slice(1);
+console.log("cognome lettera MAIUSC",cognomeCapitalized);
 
 // inserirlo in un array con altri cognomi: ‘Bianchi’, ‘Rossi’, ‘Duzioni’, ‘Balsano’, ‘Verdi’
 var listaCognomi = ["Bianchi","Rossi","Duzioni","Balsano","Verdi"];
 console.log("lista iniziale", listaCognomi);
 
-listaCognomi.push(cognomeUtente);
+listaCognomi.push(cognomeCapitalized);
 console.log("lista push", listaCognomi);
 
 // stampa la lista ordinata alfabeticamente
@@ -19,13 +21,14 @@ var i = 0;
 while (i < listaCognomi.length ) {
 
   stampaOrdinata = stampaOrdinata + "<li>" + listaCognomi[i] + "</li>";
+
   i++
 }
 
 document.getElementById('lista').innerHTML = stampaOrdinata;
 
 // scrivi anche la posizione “umana” della lista in cui il nuovo utente si trova
-var numb = listaCognomi.indexOf(cognomeUtente);
+var numb = listaCognomi.indexOf(cognomeCapitalized);
 console.log("posizione", ++numb);
 
 document.getElementById("numero").innerHTML = "il tuo cognome si trova alla posizione n° " + ++numb;
